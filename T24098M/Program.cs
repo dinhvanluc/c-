3a12cda972a057337e43e41bd4abbf434e22042e
baithi <baithi>
2025-07-21 11:49:22 +0700

    e nộp bài

---
+++ T24098M/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using T24098M.sesion2;
+
+namespace BankApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Tạo một tài khoản ngân hàng mới
+            BankAccount account = new BankAccount("123456", "Đinh Văn Lực", 5000000);
+            // In thông tin tài khoản
+            account.PrintAccountInfo();
+           
+            account.Deposit(5000);
+            // Nạp tiền vào tài khoản
+
+            account.Withdraw(2000);
+            // Rút tiền từ tài khoản
+
+            account.PrintAccountInfo();
+            // In thông tin tài khoản sau khi giao dịch
+        }
+    }
+}
---
+++ T24098M/sesion2/BankAccount.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T24098M.sesion2
+{
+    internal class BankAccount
+    {
+        public string AccountNumber {  get; set; }
+        public string OwnerName { get; set; }
+        public double Balance { get; set; }
+
+        // Constructor
+        public BankAccount(string accountNumber, string ownerName, double balance)
+        {
+            AccountNumber = accountNumber;
+            OwnerName = ownerName;
+            Balance = balance;
+        }
+
+        // nạp tiền 
+        public void Deposit(double amount)
+        {
+            if (amount > 0)
+            {
+                Balance += amount;
+                Console.WriteLine($"Deposited: {amount}. New balance: {Balance}");
+            }
+            else
+            {
+                Console.WriteLine("Deposit amount must be positive.");
+            }
+        }
+
+        // rút tiền
+        public void Withdraw(double amount)
+        {
+            if (amount > 0 && amount <= Balance)
+            {
+                Balance -= amount;
+                Console.WriteLine($"Withdrawn: {amount}. New balance: {Balance}");
+            }
+            else if (amount > Balance)
+            {
+                Console.WriteLine("Insufficient funds for withdrawal.");
+            }
+            else
+            {
+                Console.WriteLine("Withdrawal amount must be positive.");
+            }
+        }
+        // in thông tin tài khoản
+        public void PrintAccountInfo()
+        {
+            Console.WriteLine("\n---Thông tin tài khoản---- ");
+            Console.WriteLine("So tai khoan: {123456}");
+            Console.WriteLine("Chu tai khoan: {dinh văn luc}");
+            Console.WriteLine("So du: {2500000}");
+        }
+
+    }
+}
